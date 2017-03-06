6d2fbcff5c540f38c2c790130c4c03b718b33406
Kunal Gupta <kunal.gupta@appdynamics.com>
2017-03-06 13:41:24 -0800

    v3.0.2  - Changed the path to java

--- src/main/resources/bppm-alert.sh
+++ src/main/resources/bppm-alert.sh
@@ -4,4 +4,4 @@
 # -----------------------------------------------------------
 #DEBUG_OPTS="-Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=5008"
 
-../../../jre/bin/java -Dlog4j.configuration=file:conf/log4j.xml -Djavax.net.ssl.trustStore=certs/iiwscacerts.jks -Djavax.net.ssl.trustStorePassword=bmciiws $DEBUG_OPTS -DBPPM_CLIENT_HOME=. -jar bppm-alert.jar "$@"
+java -Dlog4j.configuration=file:conf/log4j.xml -Djavax.net.ssl.trustStore=certs/iiwscacerts.jks -Djavax.net.ssl.trustStorePassword=bmciiws $DEBUG_OPTS -DBPPM_CLIENT_HOME=. -jar bppm-alert.jar "$@"
